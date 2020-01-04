0b1b5d4c949a57c9a179dcbc0d1d732f50b240e8
Azeez <adenijiazeez@gmail.com>
2020-01-04 22:22:37 +0100

    feat: run_kubernetes.sh edited to configure the container
    Resolves: #8

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build -t projecthouse .
+docker build --no-cache --rm -t projecthouse .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker images
 
 # Step 3: 
 # Run flask app
-docker exec -it projecthouse bash
+docker run -p 8000:80 projecthouse
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+$dockerpath = adenijiazeez/machinelearning-micro-srv
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run projecthouse --image=$dockerpath --port=80
+kubectl expose deployment projecthouse --type=NodePort
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward adenijiazeez/machinelearning-micro-srv:latest 8000:80
 
