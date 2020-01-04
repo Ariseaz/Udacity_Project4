0acb27a8c5a9bd2eb2235905657937737d26ab17
Azeez <adenijiazeez@gmail.com>
2020-01-04 23:38:24 +0100

    feat: updates to run_kubernetes with reference to kubernetes dir
    Resolves: #12

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,12 +5,11 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-$dockerpath = adenijiazeez/machinelearning-micro-srv
+dockerpath = "adenijiazeez/machinelearning-micro-srv"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run projecthouse --image=$dockerpath --port=80
-kubectl expose deployment projecthouse --type=NodePort
+kubectl apply -f kubernetes/projecthouse.yml
 
 
 # Step 3:
