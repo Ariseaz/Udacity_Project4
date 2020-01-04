e2cbd9baf3971400ac1a303eb20306280f7a0dc4
Azeez <adenijiazeez@gmail.com>
2020-01-04 10:36:50 +0100

    feat: upadates to Dockerfile passing in more instructions
    Resolves: #1

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t projecthouse .
 
 # Step 2: 
 # List docker images
+docker images
 
 # Step 3: 
 # Run flask app
+docker exec -it projectapp bash
